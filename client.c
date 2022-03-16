222e8496a10f2db99ef494b64998cea1f77f45a7
zhngqi_ubuntu <zhengquan@micronet.ltd>
2022-03-16 10:21:41 +0800

    将聊天改为函数

--- client.c
+++ client.c
@@ -10,6 +10,39 @@
 #define server_port 88
 #define server_ip "192.168.0.128"
 
+
+void  cheat (int client_sock,struct sockaddr_in server_addr)
+{
+	int send,recv;
+	int addr_len = sizeof(server_addr);	
+	char send_buf[256];
+	char recv_buf[256];
+	//发	
+	printf("发送：");
+	scanf("%s",send_buf);
+	
+	send = sendto(client_sock,send_buf,sizeof(send_buf),0,(struct sockaddr *)&server_addr,sizeof(server_addr));
+	if(send < 0)
+	{
+		perror("发送未完成");
+		return -1;
+		
+	}
+	else 
+		printf("发送完成");
+	//收
+	recv = recvfrom(client_sock, recv_buf, sizeof(recv_buf), 0, (struct sockaddr *)&server_addr, &addr_len);	   
+	if(recv < 0)  
+	{  
+		perror("recvfrom error:");	
+		return -1;	
+	}
+	printf("接收到%d bytes；%s\n",recv,recv_buf);
+	close (client_sock);
+
+}
+
+
 int  main ()
 {
 //创建套接字client_sock
@@ -26,34 +59,10 @@ int  main ()
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_addr.s_addr = inet_addr(server_ip);
 	server_addr.sin_port = htons(server_port);
-	int addr_len = sizeof(server_addr);	
+	
 //收发准备
 
-	int send,recv;
-	char send_buf[256];
-	char recv_buf[256];
-	//发	
-	printf("发送：");
-	scanf("%s",send_buf);
-
-	send = sendto(client_sock,send_buf,sizeof(send_buf),0,(struct sockaddr *)&server_addr,sizeof(server_addr));
-	if(send < 0)
-	{
-		perror("发送未完成");
-		return -1;
-	
-	}
-	else 
-		printf("发送完成");
-	//收
-	recv = recvfrom(client_sock, recv_buf, sizeof(recv_buf), 0, (struct sockaddr *)&server_addr, &addr_len);       
-	if(recv < 0)  
-	{  
-	    	perror("recvfrom error:");  
-		return -1;  
-	}
-	printf("接收到%d bytes；%s\n",recv,recv_buf);
-	close (client_sock);
+	cheat(client_sock,server_addr);
 
 
 }
--- server.c
+++ server.c
@@ -15,6 +15,43 @@
 #define server_port  88
 #define server_IP 192.168.0.128
 
+int cheat (int sock_fd)
+{
+	
+	char recv_buf[256];
+	char send_buf[256];
+	int recv,send;
+	struct sockaddr_in addr_client;
+	int len = sizeof(addr_client);
+	//收
+		recv = recvfrom(sock_fd,recv_buf,sizeof(recv_buf),0,(struct sockaddr *) &addr_client,&len);
+		if(recv < 0)
+		{
+			perror("recvfrom error");
+			return -1;
+		}
+			
+		recv_buf[recv] = '\0';
+		printf("客户端发送%d bytes：%s\n",recv,recv_buf);
+		
+			
+	//发	
+		printf("发送:");
+		scanf("%s",send_buf);
+			
+		send = sendto(sock_fd,send_buf,sizeof(send_buf),0,(struct sockaddr* )& addr_client,len);
+			
+		if(send < 0)
+		{
+			perror("send error");
+			return -1;
+		
+		}
+
+
+}
+
+
 int main ()
 {
 	
@@ -32,49 +69,24 @@ int main ()
 	addr_server.sin_family = AF_INET;
 	addr_server.sin_port = htons(server_port);
 	addr_server.sin_addr.s_addr = htonl(INADDR_ANY);
-
 	int addr_len = sizeof(addr_server);
+
 	if(bind(sock_fd,(struct sockaddr *)&addr_server,sizeof(addr_server)) < 0)
 	{
 		perror("bind error");
 		return -1;
 	
 	}
+	
 //接收
-	char recv_buf[256];
-	char send_buf[256];
-	int recv,send;
 
-	struct sockaddr_in addr_client;
+	printf("服务器准备就绪\n");
 
 	while(1)
 	{
-		printf("服务器准备就绪\n");
-//收
-		recv = recvfrom(sock_fd,recv_buf,sizeof(recv_buf),0,(struct sockaddr *) &addr_client,&addr_len);
-		if(recv < 0)
-		{
-			perror("recvfrom error");
-			return -1;
-		}
+		cheat (sock_fd);
 		
-		recv_buf[recv] = '\0';
-		printf("客户端发送%d bytes：%s\n",recv,recv_buf);
-	
 		
-//发	
-		printf("发送:");
-		scanf("%s",send_buf);
-		
-		send = sendto(sock_fd,send_buf,sizeof(send_buf),0,
-				(struct sockaddr* )& addr_client,len);
-		
-		if(send < 0)
-		{
-			perror("send error");
-			return -1;
-		
-		}
 	}
 	close(sock_fd);
 	return 0;	
